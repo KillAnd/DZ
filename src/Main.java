4e46b1d8a9a555d413e2a2f200e124e946cdb14d
kila1 <kila14081996@gmail.com>
2024-10-05 09:53:46 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> names = new ArrayList<>(List.of(
+                "Никита",
+                "Андрей",
+                "Виталий",
+                "Анна"
+        ));
 
+        List<String> collected = names.stream()
+                .map(name -> name.substring(1))
+                .collect(Collectors.toList());
+
+        System.out.println(collected);
 
     }
 }
